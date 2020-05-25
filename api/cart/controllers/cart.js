7d7397382079761c73d776410f573eda9d7471f8
Allen Penate <allen@underbyte.net>
2020-05-24 22:40:09 -0400

    Add cart.create endpoint based on code.

--- api/cart/controllers/cart.js
+++ api/cart/controllers/cart.js
@@ -1,8 +1,22 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
+
+module.exports = {
+  /**
+   * Create a record.
+   *
+   * @return {Object}
+   */
+
+  async create(ctx) {
+    let cart = await strapi.services.cart.create(ctx.request.body);
+    return sanitizeEntity(cart, { model: strapi.models.cart });
+  }
+
+};
--- api/cart/services/cart.js
+++ api/cart/services/cart.js
@@ -4,5 +4,14 @@
  * Read the documentation (https://strapi.io/documentation/3.0.0-beta.x/concepts/services.html#core-services)
  * to customize this service
  */
+const uuid = require('uuid');
 
-module.exports = {};
+module.exports = {
+  
+  async create({code}){
+    if(!code){
+      let code = uuid();
+    }
+    return await strapi.query('cart').create({code: code});
+  }
+};
